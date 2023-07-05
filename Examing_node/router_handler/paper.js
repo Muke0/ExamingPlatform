6c339c720faf684fc2bcd8b736e306e4a3e863f6
610759768@qq.com <610759768@qq.com>
2023-07-05 15:54:37 +0800

    自动组卷

--- Examing_node/router_handler/paper.js
+++ Examing_node/router_handler/paper.js
@@ -2,6 +2,7 @@
 const db = require('../db/index')
     // 导入全局的配置文件
 const config = require('../config')
+const { json } = require('express')
 
 // 上传试卷信息的处理函数
 exports.upload_paper = (req, res) => {
@@ -26,8 +27,8 @@ exports.get_paper = (req, res) => {
         if (err) {
             console.log(err)
         } else {
-            const sql = 'select * from paper limit ' + page + ',' + size;
-            conn.query(sql, (err, result) => {
+            const sql = 'select * from paper where UId=? limit ' + page + ',' + size;
+            conn.query(sql, [req.user.UId], (err, result) => {
                 if (err) {
                     console.log(err);
                 } else {
@@ -76,6 +77,7 @@ exports.update_paper = (req, res) => {
     })
 }
 
+//手动选择题目
 exports.choose_question = (req, res) => {
     const info = req.body
     const PId = info.PId
@@ -93,20 +95,59 @@ exports.choose_question = (req, res) => {
     return res.cc('题目添加成功！', 0)
 }
 
+//自动组卷
 exports.auto_question = (req, res) => {
     const info = req.body
     const choice = info.choice
+    const gapFill = info.gapFill
+    const TorF = info.TorF
+    const shortAnswer = info.shortAnswer
         // 定义 SQL 语句，插入员工信息
-    const sqlStr = 'insert into qp(QId,PId) values(?,?)'
-    db.query(sqlStr, [info.PName, info.PInformation, req.user.UId], (err, results) => {
+    const sqlStr = 'select QId,Type from question where QBId in (?)'
+    var allQuestions;
+    db.query(sqlStr, [info.QBId], (err, results) => {
         // 执行 SQL 语句失败
         if (err) {
             return res.cc(err)
         }
-        res.cc('试卷添加成功！', 0)
+        // allQuestions = results;
+        // console.log(allQuestions)
+        allQuestions = JSON.parse(JSON.stringify(results));
+        var i = 0,
+            ic = 0,
+            ig = 0,
+            it = 0,
+            is = 0;
+        var choosedQuestions = [];
+        var choosedQuestion;
+        while (ic < choice || ig < gapFill || it < TorF || is < shortAnswer) {
+            choosed = parseInt(Math.floor(Math.random() * (allQuestions.length + 1)));
+            choosedQuestion = allQuestions[choosed];
+            choosedQuestions.push(choosedQuestion)
+            allQuestions.splice(choosed, 1)
+            Type = choosedQuestion.Type
+            if (Type == 'choice') {
+                ic = ic + 1;
+            }
+            if (Type == 'gapFill') {
+                ig = ig + 1;
+            }
+            if (Type == 'TorF') {
+                it = it + 1;
+            }
+            if (Type == 'shortAnswer') {
+                is = is + 1;
+            }
+            i = i + 1;
+            if (i > 10000) {
+                res.cc('题目数量过少！', 1)
+            }
+        }
+        return res.json(choosedQuestions)
     })
 }
 
+//获取一张试卷中的所有题目信息
 exports.get_paper_question = (req, res) => {
     page = req.query.page || 0;
     size = req.query.size || 1000;
@@ -127,6 +168,7 @@ exports.get_paper_question = (req, res) => {
     })
 }
 
+//删除一张试卷中的题目
 exports.delete_paper_question = (req, res) => {
     const info = req.body
     const PId = info.PId
